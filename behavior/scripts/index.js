d5d649bd88f3425f2813fdf0748ad6d62de75311
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2016-12-09 23:14:30 +0200

    added weather steps

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,26 +1,35 @@
-'use strict'
+'use strict';
+
+const firstOfEntityRole = function (message, entity, role) {
+  role = role || 'generic';
+
+  const slots = message.slots;
+  const entityValues = slots[entity];
+  const valsForRole = entityValues ? entityValues.values_by_role[role] : null;
+
+  return valsForRole ? valsForRole[0] : null
+};
 
 exports.handle = (client) => {
-  // Create steps
   const sayHello = client.createStep({
     satisfied() {
       return Boolean(client.getConversationState().helloSent)
     },
 
     prompt() {
-      client.addResponse('welcome')
+      client.addResponse('welcome');
       client.addResponse('provide/documentation', {
         documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('provide/instructions')
+      });
+      client.addResponse('provide/instructions');
 
       client.updateConversationState({
         helloSent: true
-      })
+      });
 
       client.done()
     }
-  })
+  });
 
   const untrained = client.createStep({
     satisfied() {
@@ -28,22 +37,55 @@ exports.handle = (client) => {
     },
 
     prompt() {
-      client.addResponse('apology/untrained')
+      client.addResponse('apology/untrained');
       client.done()
     }
-  })
+  });
+
+  const collectCity = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().weatherCity)
+    },
+    extractInfo() {
+      const city = firstOfEntityRole(client.getMessagePart(), 'city');
+
+      if (city) {
+        client.updateConversationState({
+          weatherCity: city,
+        });
+
+        console.log('User wants the weather in:', city.value);
+      }
+    },
+    prompt() {
+      client.addResponse('app:response:name:prompt/weather_city');
+      client.done();
+    },
+  });
+
+  const provideWeather = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      let weatherData = {
+        temperature: 60,
+        condition: 'sunny',
+        city: client.getConversationState().weatherCity.value,
+      };
+
+      client.addResponse('app:response:name:provide_weather/current', weatherData);
+      client.done();
+    },
+  });
 
   client.runFlow({
-    classifications: {
-      // map inbound message classifications to names of streams
-    },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
-    },
+    classifications: {},
     streams: {
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained],
-    },
-  })
-}
+      main: 'getWeather',
+      hi: [sayHello],
+      getWeather: [collectCity, provideWeather],
+    }
+  });
+};
